b79da2dbe64a0aab2f9be78152aef83eac443ce1
OscarElliott <90659276+OscarElliott@users.noreply.github.com>
2023-03-03 19:03:42 +1030

    :<

--- main.cpp
+++ main.cpp
@@ -11,7 +11,13 @@ int main() {
     Player* computer = new Computer();
 
     for (int i = 0; i < 3; i++) {
-        ref.refGame(human, computer);
+        Player * winner = ref.refGame(human, computer);
+        if(winner == nullptr) {
+            std::cout << "Tie" << std::endl;
+        }
+        else {
+            std::cout << winner->getName() << " wins" << std::endl;
+        }
         std::cout << std::endl;
     }
 
