d2e156448c3971b0e2c1e31749a52b413151a7be
OscarElliott <90659276+OscarElliott@users.noreply.github.com>
2023-03-03 13:18:42 +1030

    fixed referee

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include "Player.h"
+#include "Computer.h"
+#include "Human.h"
+#include "Referee.h"
+
+
+
+int main() {
+    Referee ref;
+    Player* human = new Human();
+    Player* computer = new Computer();
+
+    for (int i = 0; i < 3; i++) {
+        ref.refGame(human, computer);
+        cout << endl;
+    }
+
+    delete human;
+    delete computer;
+
+    return 0;
+}
