0961978072003bf8fbf0160c2d22204960058544
OscarElliott <90659276+OscarElliott@users.noreply.github.com>
2023-03-03 13:01:15 +1030

    basic class outlines

---
+++ Referee.h
@@ -0,0 +1,20 @@
+#ifndef o_h
+#define o_h
+
+#include <iostream>
+#include <string>
+
+
+class Referee{
+
+    public:
+
+    Winner *winner
+
+    Referee(); //default constructor
+    Player * refGame(Player * player1, Player * player2)
+    // returns the reference to the winning player
+
+}
+
+#endif
