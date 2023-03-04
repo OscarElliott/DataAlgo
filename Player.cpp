0a1066b63fc9b9a57f758394c17caa6228093449
OscarElliott <90659276+OscarElliott@users.noreply.github.com>
2023-03-04 19:15:31 +1030

    fixed player

--- Player.cpp
+++ Player.cpp
@@ -1,12 +1,3 @@
 // This is the parent class to Computer and Human it will provide framework for the game
+#include "Player.h"
 
-#include <iostream>
-
-class Player{
-
-    public:
-
-    virtual char makeMove( ) = 0;
-    virtual std::string getName( ) = 0;
-    
-};
--- Player.h
+++ Player.h
@@ -5,8 +5,8 @@
 
 class Player{
 
-    virtual char makeMove( );
-    virtual std::string getName( );
+    virtual char makeMove( ) = 0;
+    virtual std::string getName( ) = 0;
     
 };
 
