01acad2b3b0339cbc8aa292cd348da31b018a30e
OscarElliott <90659276+OscarElliott@users.noreply.github.com>
2023-03-08 19:13:33 +1030

    f

--- Practical2/Paper.h
+++ Practical2/Paper.h
@@ -10,7 +10,7 @@
 class Paper: public Move {
     private:
         std::string name; //name of Move
-        std::vector<Move> victory // list of moves this move beats
+        std::vector<Move> victory; // list of moves this move beats
         std::vector<Move> defeat; // list of moves this move losses to
     public:
         Paper(std::string n = "Paper"); // Paper constructor, default name for paper is paper
