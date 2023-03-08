99659965e11016fda70410b024c4f6825c11a43e
OscarElliott <90659276+OscarElliott@users.noreply.github.com>
2023-03-08 14:30:22 +1030

    :)

--- Practical2/Move.h
+++ Practical2/Move.h
@@ -11,6 +11,7 @@ class Move{
         std::vector<Move> defeat; // list of moves this move losses to
     public:
         std::string getName(); //returns the name of a Move instance, for example "Ninja".  This function exists purely so I can do some tests.  I don't expect you to call it in your code other than while testing.
+        std::string setName(char newname); // Allows to set name which is private with public function
         std::vector<Move> getWins(); // returns list of moves given move beats
         std::vector<Move> getLosses(); // returns list of moves given move losses too
 };
